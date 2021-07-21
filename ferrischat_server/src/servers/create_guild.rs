cf62230bea842002205934d7c1a5985f0078f5eb
0/0 <zero@imaskeleton.me>
2021-07-20 22:45:44 -0600

    add required type

--- ferrischat_server/src/servers/create_guild.rs
+++ ferrischat_server/src/servers/create_guild.rs
@@ -1,9 +1,10 @@
 use rocket::http::Status;
 use rocket::response::{content, status};
+use rocket::Route;
 
 #[post("/")]
 pub async fn create_guild() -> status::Custom<&'static str> {
     status::Custom(Status::Created, "created guild test")
 }
 
-pub const CREATE_GUILD_ROUTES: _ = routes![create_guild];
+pub const CREATE_GUILD_ROUTES: Vec<Route> = routes![create_guild];
