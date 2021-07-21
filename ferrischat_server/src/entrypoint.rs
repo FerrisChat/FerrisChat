496695b114b94e550d88541644bb475792760ad3
0/0 <zero@imaskeleton.me>
2021-07-20 22:51:44 -0600

    how about we just refactor it all

--- ferrischat_server/src/entrypoint.rs
+++ ferrischat_server/src/entrypoint.rs
@@ -1,6 +1,11 @@
 use rocket::{Rocket, Ignite};
-use crate::servers::create_guild::CREATE_GUILD_ROUTES;
+use crate::servers::create_guild::create_guild;
+use rocket::response::status;
+
+async fn private_create_guild() -> status::Custom<&'static str> {
+    create_guild()
+}
 
 pub async fn entrypoint() {
-    rocket::build().mount("/api/v1/guilds", CREATE_GUILD_ROUTES).launch().await.expect("failed to launch rocket!")
+    rocket::build().mount("/api/v1/guilds", routes![private_create_guild]).launch().await.expect("failed to launch rocket!")
 }
--- ferrischat_server/src/servers/create_guild.rs
+++ ferrischat_server/src/servers/create_guild.rs
@@ -1,10 +1,6 @@
 use rocket::http::Status;
 use rocket::response::{content, status};
-use rocket::Route;
 
-#[post("/")]
 pub async fn create_guild() -> status::Custom<&'static str> {
     status::Custom(Status::Created, "created guild test")
 }
-
-pub const CREATE_GUILD_ROUTES: Vec<Route> = routes![create_guild];
