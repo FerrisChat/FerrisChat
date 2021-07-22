8addb8fc9cf8b08ebab5f51861252fe70024f2ac
hydrostaticcog <hydro@hydrostaticcog.me>
2021-07-21 22:23:57 -0500

    add the rest

--- ferrischat_server/src/entrypoint.rs
+++ ferrischat_server/src/entrypoint.rs
@@ -1,6 +1,8 @@
 use crate::guilds::*;
 use crate::users::*;
 use crate::channels::*;
+use crate::members::*;
+use crate::messages::*;
 use rocket::response::status;
 use rocket::{Ignite, Rocket};
 use ferrischat_db::load_db;
@@ -33,6 +35,22 @@ async fn private_create_channel() -> status::Custom<&'static str> { create_chann
 /// GET `/api/v1/channels/<id>`
 async fn private_get_channel(id: u64) -> status::Custom<&'static str> { get_channel(id).await }
 
+#[post("/")]
+/// POST `/api/v1/message/`
+async fn private_send_message() -> status::Custom<&'static str> { send_message().await }
+
+#[get("/<id>")]
+/// GET `/api/v1/message/<id>`
+async fn private_get_message(id: u64) -> status::Custom<&'static str> { get_message(id).await }
+
+#[post("/")]
+/// POST `/api/v1/member/`
+async fn private_create_member() -> status::Custom<&'static str> { create_member().await }
+
+#[get("/<id>")]
+/// GET `/api/v1/member/<id>`
+async fn private_get_member(id: u64) -> status::Custom<&'static str> { get_member(id).await }
+
 
 pub async fn entrypoint() {
     load_db().await;
@@ -41,6 +59,8 @@ pub async fn entrypoint() {
         .mount("/api/v1/guilds", routes![private_create_guild, private_get_guild])
         .mount("/api/v1/user", routes![private_create_user, private_get_user])
         .mount("/api/v1/channels", routes![private_create_channel, private_get_channel])
+        .mount("/api/v1/message", routes![private_send_message, private_get_message])
+        .mount("/api/v1/member", routes![private_create_member, private_get_member])
         .launch()
         .await
         .expect("failed to launch rocket!")
---
+++ ferrischat_server/src/messages/mod.rs
@@ -0,0 +1,5 @@
+mod get_messages;
+mod send_message;
+
+pub use get_messages::*;
+pub use send_message::*;
