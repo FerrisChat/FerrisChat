cedfc29a03de8aa87209cf44b8db5c6f5a8a8ac8
hydrostaticcog <hydro@hydrostaticcog.me>
2021-07-21 22:11:56 -0500

    add channels and user endpoints

--- ferrischat_server/src/entrypoint.rs
+++ ferrischat_server/src/entrypoint.rs
@@ -1,4 +1,6 @@
-use crate::servers::*;
+use crate::guilds::*;
+use crate::users::*;
+use crate::channels::*;
 use rocket::response::status;
 use rocket::{Ignite, Rocket};
 use ferrischat_db::load_db;
@@ -15,12 +17,30 @@ async fn private_get_guild(id: u64) -> status::Custom<&'static str> {
     get_guild(id).await
 }
 
+#[post("/")]
+/// POST `/api/v1/users/`
+async fn private_create_user() -> status::Custom<&'static str> { create_user().await }
+
+#[get("/<id>")]
+/// GET `/api/v1/users/<id>`
+async fn private_get_user(id: u64) -> status::Custom<&'static str> { get_user(id).await }
+
+#[post("/")]
+/// POST `/api/v1/channels/`
+async fn private_create_channel() -> status::Custom<&'static str> { create_channel().await }
+
+#[get("/<id>")]
+/// GET `/api/v1/channels/<id>`
+async fn private_get_channel(id: u64) -> status::Custom<&'static str> { get_channel(id).await }
+
 
 pub async fn entrypoint() {
     load_db().await;
 
     rocket::build()
         .mount("/api/v1/guilds", routes![private_create_guild, private_get_guild])
+        .mount("/api/v1/user", routes![private_create_user, private_get_user])
+        .mount("/api/v1/channels", routes![private_create_channel, private_get_channel])
         .launch()
         .await
         .expect("failed to launch rocket!")
